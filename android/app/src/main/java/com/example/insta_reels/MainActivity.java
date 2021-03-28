ae6974b6e20e45ff15797806c5cac68592b7360e
Ankita Kumari <Ankitak28@gmail.com>
2021-03-28 17:10:19 +0530

    Insta reels repository

---
+++ android/app/src/main/java/com/example/insta_reels/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.insta_reels;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Insta Reels App UI",
+    home : instaReels()
+  ));
+}
+class instaReels extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
