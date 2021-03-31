ee371102c369ba64789b916435f4a9ce8991b9f6
Ankita Kumari <Ankitak28@gmail.com>
2021-03-31 14:38:46 +0530

    Bottom navigation bar made transparent and defaultTabController implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ void main() {
   runApp(MaterialApp(
     title: "Insta Reels App UI",
     home: insta_reels(),
-    theme: ThemeData(primarySwatch: Colors.blue),
+    theme:
+        ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.transparent),
     debugShowCheckedModeBanner: false,
   ));
 }
@@ -21,36 +22,49 @@ class insta_reels_state extends State<insta_reels> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  backgroundColor: Colors.black,
-                  label: "Home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  backgroundColor: Colors.black,
-                  label: "Discover"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.add),
-                  label: "Add",
-                  backgroundColor: Colors.black),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.message),
-                  label: "Inbox",
-                  backgroundColor: Colors.black),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.account_circle),
-                  label: "Me",
-                  backgroundColor: Colors.black),
-            ],
-            type: BottomNavigationBarType.shifting,
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.grey,
-            onTap: _onItemTap),
-        body: Container());
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            extendBody: true,
+            extendBodyBehindAppBar: true,
+            backgroundColor: Colors.black,
+            bottomNavigationBar: BottomNavigationBar(
+                elevation: 0,
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: "Home"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.search), label: "Discover"),
+                  BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.message), label: "Inbox"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.account_circle), label: "Me"),
+                ],
+                type: BottomNavigationBarType.shifting,
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.white,
+                unselectedItemColor: Colors.grey,
+                onTap: _onItemTap),
+            body: TabBarView(
+              children: [
+                Center(
+                    child: Text(
+                  "anki",
+                  style: TextStyle(color: Colors.blue),
+                )),
+                Center(
+                    child: Text(
+                  "sweeety",
+                  style: TextStyle(color: Colors.blue),
+                )),
+                Center(
+                    child: Text(
+                  "Bitto",
+                  style: TextStyle(color: Colors.blue),
+                ))
+              ],
+            )));
   }
 
   void _onItemTap(int index) {
